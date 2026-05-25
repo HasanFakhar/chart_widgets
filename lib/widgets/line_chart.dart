2c891656c79e530e1dcedbf189c7bfc0596aec43
Hasan Fakhar <hfakhar.bese21seecs@seecs.edu.pk>
2026-05-25 12:00:42 +0400

    fetch data and display

--- lib/widgets/line_chart.dart
+++ lib/widgets/line_chart.dart
@@ -5,14 +5,51 @@ import '../controllers/product_controller.dart';
 
 
 class MyLineChart extends StatefulWidget {
+
+    ProductController productController = ProductController();
+    String category; // will be used to fetch products by category and display in the chart
+    bool prices; // if true, show price data; if false, show rating data
+
     
-    const MyLineChart({super.key,});
+    MyLineChart({super.key, this.category='all', this.prices=true});
 
   @override
   State<MyLineChart> createState() => _MyLineChartState();
 }
 
 class _MyLineChartState extends State<MyLineChart> {
+ Map<String, double> dataPoints = {}; // to hold the data points for the chart
+
+void fetchData() async {
+    try {
+      List products;
+      if (widget.category == 'all') {
+        products = await widget.productController.fetchAllProducts();
+      } else {
+        products = await widget.productController.fetchByCategory(widget.category);
+      }
+      
+      setState(() {
+          dataPoints = {
+            for (var product in products) 
+              product.title : widget.prices
+                  ? product.price.toDouble()
+                  : product.rating.toDouble(),
+          };
+        });
+
+    } catch (e) {
+      print('Error fetching data: $e');
+    }
+
+    print('Data points: $dataPoints'); // Debug print to check the data points
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData(); // Fetch data when the widget is initialized
+  }
   
   @override
   Widget build(BuildContext context) {
@@ -20,13 +57,12 @@ class _MyLineChartState extends State<MyLineChart> {
       LineChartData(
         lineBarsData: [
           LineChartBarData(
-            spots: [
-              FlSpot(0, 1),
-              FlSpot(1, 3),
-              FlSpot(2, 2),
-              FlSpot(3, 5),
-              FlSpot(4, 4),
-            ],
+            spots: dataPoints.entries
+                .map((entry) => FlSpot(
+                      dataPoints.keys.toList().indexOf(entry.key).toDouble(),
+                      entry.value,
+                    ))
+                .toList(),
             isCurved: true,
             color: Colors.blue,
             barWidth: 3,
