98c5bf0b9d29c2248c527f8ae43445d4396f4e9b
Hasan Fakhar <hfakhar.bese21seecs@seecs.edu.pk>
2026-05-25 09:49:43 +0400

    reuse models and services

---
+++ lib/controllers/product_controller.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/product_model.dart';
+import '../services/get_products.dart';
+
+class ProductController {
+
+  final ProductService _productService = ProductService();
+
+  Future<List<ProductModel>> fetchAllProducts() async {
+    try {
+      return await _productService.getAllProducts();
+    } catch (e) {
+      throw Exception('Error fetching products: $e');
+    }
+  }
+
+  Future<List<ProductModel>> fetchSmartphones() async {
+    try {
+      return await _productService.getSmartphones();
+    } catch (e) {
+      throw Exception('Error fetching smartphones: $e');
+    }
+  }
+
+  Future<List<ProductModel>> fetchLaptops() async {
+    try {
+      return await _productService.getLaptops();
+    } catch (e) {
+      throw Exception('Error fetching laptops: $e');
+    }
+  }
+
+}
---
+++ lib/services/get_products.dart
@@ -0,0 +1,81 @@
+import 'package:chart_widgets/models/product_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class ProductService {
+  static const String _baseUrl = 'https://dummyjson.com';
+
+
+  Future<List<ProductModel>> _fetchProducts(String url) async {
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return List<ProductModel>.from(
+        data['products'].map((item) => _mapProduct(item)),
+      );
+    } else {
+      throw Exception('Failed to load products');
+    }
+  }
+
+
+  Future<List<ProductModel>> getAllProducts() async {
+    return _fetchProducts('$_baseUrl/products');
+  }
+
+
+  Future<List<ProductModel>> getSmartphones() async {
+    return _fetchProducts('$_baseUrl/products/category/smartphones');
+  }
+
+
+  Future<List<ProductModel>> getLaptops() async {
+    return _fetchProducts('$_baseUrl/products/category/laptops');
+  }
+
+
+  // Map JSON to ProductModel
+  ProductModel _mapProduct(Map<String, dynamic> item) {
+    return ProductModel(
+      id: item['id'],
+      title: item['title'],
+      description: item['description'],
+      category: item['category'],
+      price: (item['price'] as num).toDouble(),
+      discountPercentage: (item['discountPercentage'] as num).toDouble(),
+      rating: (item['rating'] as num).toDouble(),
+      stock: item['stock'],
+      tags: List<String>.from(item['tags']),
+      brand: item['brand'],
+      sku: item['sku'] ,
+      weight: item['weight'] ,
+      dimensions: Dimensions(
+        width: (item['dimensions']['width'] as num).toDouble(),
+        height: (item['dimensions']['height'] as num).toDouble(),
+        depth: (item['dimensions']['depth'] as num).toDouble(),
+      ),
+      warrantyInformation: item['warrantyInformation'] ,
+      shippingInformation: item['shippingInformation'] ,
+      availabilityStatus: item['availabilityStatus'] ,
+      reviews: List<Review>.from(
+        (item['reviews'] ?? []).map((review) => Review(
+          reviewerName: review['reviewerName'],
+          rating: review['rating'],
+          comment: review['comment'],
+          date: DateTime.parse(review['date']),
+          reviewerEmail: review['reviewerEmail'],
+        )),
+      ),
+      returnPolicy: item['returnPolicy'] ,
+      minimumOrderQuantity: item['minimumOrderQuantity'],
+      meta: Meta(
+        createdAt: DateTime.parse(item['meta']['createdAt']),
+        updatedAt: DateTime.parse(item['meta']['updatedAt']),
+        barcode: item['meta']['barcode'],
+        qrCode: item['meta']['qrCode'],
+      ),
+      images: List<String>.from(item['images']),
+      thumbnail: item['thumbnail'],
+    );
+  }
+}
